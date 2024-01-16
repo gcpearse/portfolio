b62ac0f0d065a02faa4be679067fa6366e047506
gcpearse <glen.c.pearse@gmail.com>
2024-01-16 17:39:33 +0000

    creates models file for custom types

---
+++ src/models.ts
@@ -0,0 +1,10 @@
+export type Project = {
+  title: string
+  tagline: string
+  type: string
+  isHosted: boolean
+  siteLink: string | null
+  repository: string
+  techStack: string[]
+  description: string
+}
