5a9fc7e6404ca4402271d352a4b3d38419b6abbf
gcpearse <glen.c.pearse@gmail.com>
2024-01-17 00:02:44 +0000

    builds nav sidebar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,32 @@
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { IoClose, IoMenu } from "react-icons/io5"
+import { toggle } from '../features/nav/navSlice'
+
 const Nav: React.FC = () => {
+
+  const isToggled = useAppSelector((state) => state.nav.isToggled)
+  const dispatch = useAppDispatch()
+
   return (
-    <nav>
+    <nav className={!isToggled ? (
+      'sidebar'
+    ) : (
+      'sidebar open-nav'
+    )}>
+      {!isToggled ? (
+        <IoMenu
+          className='menu-icon'
+          onClick={() => dispatch(toggle())} />
+      ) : (
+        <IoClose
+        className='menu-icon'
+        onClick={() => dispatch(toggle())} />
+      )}
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>GitHub</li>
+      </ul>
     </nav>
   )
 }
