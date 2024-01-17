a324015d95b7c1c831960ec8ec202f9aa5ded90b
gcpearse <glen.c.pearse@gmail.com>
2024-01-17 00:02:12 +0000

    creates navSlice

---
+++ src/features/nav/navSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+type NavState = {
+  isToggled: boolean
+}
+
+const initialState: NavState = {
+  isToggled: false
+}
+
+const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    toggle: (state) => {
+      state.isToggled = !state.isToggled
+    }
+  }
+})
+
+export const { toggle } = navSlice.actions
+
+export default navSlice.reducer
