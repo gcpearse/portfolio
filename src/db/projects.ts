c0f9ee63b3f5fc0ac2518fb71f163f37ed543045
gcpearse <glen.c.pearse@gmail.com>
2024-01-18 15:57:41 +0000

    adds three more projects to the array

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -55,5 +55,54 @@ export const projects: Project[] = [
       "AWS"
     ],
     description: "Umami is a collaborative recipe site on which users can not only view and post recipes, but also 'fork' (copy) existing recipes, make edits, and then submit their amended version of a recipe to the site. The application offers version control for the cooking community: in essence, it is the culinary world's answer to GitHub."
+  },
+  {
+    projectId: 4,
+    title: "CarboMonitor",
+    tagline: "API interface for the Official Carbon Intensity API",
+    type: "front-end",
+    isHosted: true,
+    imgLink: "carbomonitor.png",
+    siteLink: "https://carbomonitor.netlify.app/",
+    repository: "https://github.com/gcpearse/carbon-intensity-api-interface",
+    techStack: [
+      "JavaScript",
+      "React",
+      "Vite",
+      "Axios"
+    ],
+    description: "CarboMonitor makes requests to the National Grid's Official Carbon Intensity API. This mini project helped me get to grips with using Axios in React to interact with an API and clearly display relevant data on the front end."
+  },
+  {
+    projectId: 5,
+    title: "Taskyard",
+    tagline: "A basic to-do list app built in TypeScript",
+    type: "front-end",
+    isHosted: true,
+    imgLink: "taskyard.png",
+    siteLink: "https://taskyard.netlify.app/",
+    repository: "https://github.com/gcpearse/to-do-list-typescript",
+    techStack: [
+      "TypeScript",
+      "React",
+      "Vite"
+    ],
+    description: "Taskyard was built by following Piyush Agarwal's YouTube tutorial for freeCodeCamp.org. The majority of the functionality in Taskyard is shared with Piyush's Taskify app, but there are some additional features. The app allows users to add, edit, delete, or mark tasks as complete, as well as clearing the entire list to start afresh. I used this tutorial to cover the basics of building React apps in TypeScript." 
+  },
+  {
+    projectId: 6,
+    title: "Name Roulette",
+    tagline: "A random name selector built in TypeScript",
+    type: "front-end",
+    isHosted: true,
+    imgLink: "name-roulette.png",
+    siteLink: "https://name-roulette.netlify.app/",
+    repository: "https://github.com/gcpearse/random-name-selector",
+    techStack: [
+      "TypeScript",
+      "React",
+      "Vite"
+    ],
+    description: "Name Roulette is a mini app that allows users to add names to a list, then select one of those names at random. I built this app to practise using TypeScript for React applications, focussing on functionality over aesthetic appeal." 
   }
 ]
