35eed72c7d3c47243e3e71690e9df5404805f2e9
gcpearse <glen.c.pearse@gmail.com>
2024-01-18 04:05:01 +0000

    updates text content on Home page

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Home: React.FC = () => {
   return (
     <section>
@@ -9,6 +11,7 @@ const Home: React.FC = () => {
             <h2>Junior Software Developer</h2>
             <p>Software development allows me to channel my creativity and express my imagination in a precise way. There is something immensely satisfying about seeing an application take shape before your eyes, and knowing that you built it from the ground up.</p>
             <p>Above all, what I love about development is that each day is an opportunity to learn something new in a complex, fast-paced, and relentlessly innovative field.</p>
+            <p>For each project on this site, my goal has been to create elegant, user-friendly designs supported by clean, readable code.</p>
           </div>
         </div>
         <div className="img-wrapper">
@@ -19,7 +22,9 @@ const Home: React.FC = () => {
         </div>
       </article>
       <div className="btn-wrapper">
-        <button data-front="View my projects" data-back="Go">View my projects</button>
+        <Link to="/projects">
+          <button>View my projects</button>
+        </Link>
       </div>
     </section>
   )
