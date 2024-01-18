a55655af67a0af4b97a5470f8523292923f91a21
gcpearse <glen.c.pearse@gmail.com>
2024-01-18 20:26:43 +0000

    adds scroll to top button with logic to ensure it is only shown after a given range of scrolling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import { LuChevronUp } from "react-icons/lu";
+
 const Footer: React.FC = () => {
+
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 1000) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
+  })
+
   return (
-    <footer></footer>
+    <footer>
+      <button className={!isVisible ? (
+        "scroll-up-btn"
+      ) : (
+        "scroll-up-btn visible-btn"
+      )} onClick={() => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      }}><LuChevronUp /></button>
+    </footer>
   )
 }
 
