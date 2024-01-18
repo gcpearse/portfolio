4f7c9bfaf43e4fce3001fe5bd0bf4943b7f4bb44
gcpearse <glen.c.pearse@gmail.com>
2024-01-18 01:36:15 +0000

    imports Contents component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import Contents from "./components/Contents"
 import Nav from "./components/Nav"
 
 const App: React.FC = () => {
   return (
     <>
       <Nav />
+      <Contents />
     </>
   )
 }
