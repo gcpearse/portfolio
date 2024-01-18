61f0f5abb4c8e45dd9ff5a1072e9cc5ac90f66b0
gcpearse <glen.c.pearse@gmail.com>
2024-01-18 02:20:51 +0000

    imports Header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css"
 import Footer from "./components/Footer"
 import Home from "./components/Home"
 import Nav from "./components/Nav"
+import Header from "./components/Header"
 
 const App: React.FC = () => {
   return (
     <>
       <Nav />
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
