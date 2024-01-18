cfdb255ba4177338017435b54a7b6f15fa0e7f8b
gcpearse <glen.c.pearse@gmail.com>
2024-01-18 16:40:11 +0000

    sidebar collapses when user clicks / touches elsewhere

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import Home from "./components/pages/home/Home"
 import Nav from "./components/Nav"
 import Header from "./components/Header"
 import Projects from "./components/pages/projects/Projects"
+import { useAppDispatch } from "./app/hooks"
+import { close } from "./features/nav/navSlice"
 
 const App: React.FC = () => {
+
+  const dispatch = useAppDispatch()
+
   return (
     <>
       <Nav />
       <Header />
-      <main>
+      <main onClick={() => dispatch(close())}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
